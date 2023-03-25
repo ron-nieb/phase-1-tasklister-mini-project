6f0dc6191d3bcd1f61cbc1c8c31f83576e60b9ec
Ronald Barasa <barasaogoro@gmail.com>
2023-03-25 09:06:25 +0300

    Added listeners and a delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+  taskForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const newTask = document.createElement("li");
+  newTask.innerText = event.target["new-task-description"].value;
+  //added the delete function
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", function() {
+  newTask.remove();
+  });
+  //return
+  newTask.appendChild(deleteButton);
+  taskList.appendChild(newTask);
+  });
+  });
+  
